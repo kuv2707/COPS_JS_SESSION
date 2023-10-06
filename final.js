a5a3aa8baf20b8cd0ec1f5474dd1f38fd05f8b9e
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-10-06 13:49:13 +0530

    created template

---
+++ final.js
@@ -0,0 +1,103 @@
+/**
+ * create an array for tasks
+ * define a class for task
+ * select relevant elements from DOM
+ * add event listener for add button and enter key
+ * add event listener for input field, to disable button when input is empty
+ *
+ */
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+class Task {
+	constructor(title) {
+		this.title = title;
+		this.done = false;
+		this.id = Date.now();
+	}
+}
+
+const tasksContainer = document.querySelector(".tasks-container");
+const taskInputContainer = document.querySelector(".task-input-container");
+const taskAddButton = document.querySelector(".task-add-button");
+const taskTitleInput = document.querySelector(".task-title-input");
+
+function taskAddHandler() {
+	const title = taskTitleInput.value;
+	taskTitleInput.value = "";
+	const task = new Task(title);
+	appendNewTask(task);
+}
+
+taskAddButton.addEventListener("click", taskAddHandler);
+
+taskTitleInput.addEventListener("keydown", function (e) {
+	//cannot use keypress as it does not work for backspace
+	if (taskTitleInput.value === "") {
+		taskAddButton.disabled = true;
+	} else {
+		taskAddButton.disabled = false;
+
+		if (e.key === "Enter") {
+			taskAddHandler();
+		}
+	}
+});
+
+/**
+ * Add the task object to tasks array, save it, and render it on the dom
+ * @param {Task} task The task object
+ */
+function appendNewTask(task) {
+	tasks.push(task);
+	saveTasks();
+	const taskEntryElement = createTaskEntryElement(task);
+	tasksContainer.appendChild(taskEntryElement);
+}
+
+/**
+ * should be called each time there is a change in the tasks array or its elements
+ */
+function saveTasks() {
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function createTaskEntryElement(task) {
+	const outerdiv = document.createElement("div");
+	outerdiv.className = "task-entry";
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.checked = task.done;
+	const titleshower = document.createElement("div");
+	titleshower.className = "task-title";
+	if (task.done) titleshower.classList.toggle("strike-through");
+	titleshower.innerText = task.title;
+	const deletebutton = document.createElement("button");
+	deletebutton.className = "task-delete";
+	deletebutton.innerText = "delete";
+	outerdiv.appendChild(checkbox);
+	outerdiv.appendChild(titleshower);
+	outerdiv.appendChild(deletebutton);
+
+	deletebutton.onclick = function () {
+		outerdiv.remove();
+		tasks = tasks.filter((t) => t.id !== task.id);
+		saveTasks();
+	};
+
+	checkbox.onclick = function () {
+		task.done = checkbox.checked;
+		titleshower.classList.toggle("strike-through");
+		saveTasks();
+	};
+
+	return outerdiv;
+}
+
+//when page loads, populate the task list from local storage
+window.onload = function () {
+	tasks.forEach(function (task) {
+		const taskEntryElement = createTaskEntryElement(task);
+		tasksContainer.appendChild(taskEntryElement);
+	});
+};
